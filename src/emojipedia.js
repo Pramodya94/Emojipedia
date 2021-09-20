b254048784aa293223d8eee5c6c4ee0139e7728b
Pramodya94 <76614323+Pramodya94@users.noreply.github.com>
2021-09-20 16:32:28 +0530

    Include React.js mapping to avoid repetitive codes.

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -19,5 +19,28 @@ const emojipedia = [
     name: "Rolling On The Floor, Laughing",
     meaning:
       "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😶",
+    name: "Face Without Mouth",
+    meaning:
+      "Emoji without a mouth. Is speechless, has no words. Someone does not want to or cannot say anything about a specific topic. Used in difficult, embarrassing or bad conversations. Imagine what it would say if it had a mouth!"
+  },
+  {
+    id: 5,
+    emoji: "🙃",
+    name: "Upside-Down Face",
+    meaning:
+      "Don't take me seriously! The message is either ambiguous, ironic or joking. Person plays the fool and horses around, or in response to a joke."
+  },
+  {
+    id: 6,
+    emoji: "🧐",
+    name: "Face With Monocle",
+    meaning:
+      "A monocle is a visual aid with only one glass. At the end of the 19th century it was a status symbol. Something seems suspicious! The smiley makes a warning look around. The “upper-class“ version of the pondering smiley."
   }
 ];
+
+export default emojipedia;
